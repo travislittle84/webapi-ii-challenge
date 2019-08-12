304b21864faaece2262139d2012e5afadca8deeb
Travis Little <kingtrav@gmail.com>
2019-08-11 19:51:09 -0600

    almost mvp ready

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -6,5 +6,165 @@ const router = express.Router()
 
 //end points
 
+// GET /api/posts
+// Get all the posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved.'
+        })
+    }
+})
+
+// GET /api/posts/:id
+// Get the post with the specified ID
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const post = await Posts.findById(id)
+        console.log("post", post)
+        if(post.length < 1) {
+            throw {
+                errorCode: 404,
+                message: 'The post with the specified ID does not exist.'
+            }
+        }
+        res.status(200).json(post)
+    } catch (error) {
+        if (!error.errorCode) {
+            res.status(500).json({
+                errorMessage: "There was an error while saving the comment to the database"
+            })
+        }
+        res.status(error.errorCode).json({
+            message: error.message
+        })
+    }
+})
+
+// GET /api/posts/:id/comments
+// Get all the comments associated with the specified ID
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const { id } = req.params
+        const comments = await Posts.findPostComments(id)
+        if(comments.length < 1) {
+            throw {
+                errorCode: 404,
+                message: 'The post with the specified ID does not exist.'
+            }
+        }
+        res.status(200).json(comments)
+
+    } catch (error) {
+        if (!error.errorCode) {
+            res.status(500).json({
+                errorMessage: "There was an error while saving the comment to the database"
+            })
+        }
+        res.status(error.errorCode).json({
+            message: error.message
+        })
+    }
+})
+
+// POST api/posts
+// Create post using the information in request body
+router.post('/', async (req, res) => {
+    try {
+        const post = req.body
+        if(!post.title || !post.contents) {
+            throw {
+                errorCode: 400,
+                message: 'Please provide title and contents for the post'
+            }
+        }
+        const newPost = await Posts.insert(post)
+        res.status(201).json(newPost)
+    } catch (error) {
+        if (!error.errorCode) {
+            res.status(500).json({
+                errorMessage: "There was an error while saving the comment to the database"
+            })
+        }
+        res.status(error.errorCode).json({
+            errorMessage: error.message
+        })
+    }
+})
+
+//POST /api/posts/:id/comments
+// Create a comment for the post with the information inside the request body
+router.post('/:id/comments', async (req, res) => {
+    const { id } = req.params
+    const comment = req.body
+    try {
+        if (!comment.text) {
+            throw {
+                errorCode: 400,
+                message: "Please provide text for the comment."
+            }
+        }
+        
+        const postId = await Posts.findById(comment.post_id)
+        if (postId.length == 0) {
+            throw {
+                errorCode: 404,
+                message: 'The post with the specified ID does not exist.'
+            }
+        }
+        
+        const newComment = await Posts.insertComment(comment)
+        res.status(201).json(newComment)
+
+    } catch (error) {
+        if (!error.errorCode) {
+            res.status(500).json({
+                errorMessage: "There was an error while saving the comment to the database"
+            })
+        }
+        res.status(error.errorCode).json({
+            errorMessage: error.message
+        })
+    }
+})
+
+// DELETE /api/posts/:id
+// Destroys the post with the specified ID and returns the deleted post object
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedPost = await Posts.findById(id)
+        if(deletedPost.length == 0) {
+            throw {
+                errorCode: 404,
+                message: "The post with the specified ID does not exist."
+            }
+        }
+        await Posts.remove(id)
+        res.status(200).json(deletedPost)
+
+    } catch (error) {
+        if (!error.errorCode) {
+            res.status(500).json({
+                errorMessage: "The post could not be removed"
+            })
+        }
+        res.status(error.errorCode).json({
+            errorMessage: error.message
+        })
+    }
+})
+
+
+// PUT /api/posts/:id
+// Updates the post with the specified ID using data from the request body. Returns the modified document, not the original
+
+
+
+
 
 module.exports = router
