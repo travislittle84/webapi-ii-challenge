dd78411875d885610d5cb9ec8b105a418ce3f626
Travis Little <kingtrav@gmail.com>
2019-08-14 19:15:32 -0600

    ready

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
+
 const server = require('./server.js')
-server.listen(3000, () => {
-    console.log("Server running on port 3000")
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
